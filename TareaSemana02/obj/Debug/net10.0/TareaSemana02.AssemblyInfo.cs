a4aeb2e62c14442bc46ebf89511f9a695d7141bf
Steven684-art <sa.shiguangoa@uea.edu.ec>
2025-06-18 21:52:26 -0500

    Semana2

--- TareaSemana02/obj/Debug/net10.0/TareaSemana02.AssemblyInfo.cs
+++ TareaSemana02/obj/Debug/net10.0/TareaSemana02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaSemana02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5121556eaa21a6bb2b7cb618e8fee3a2c78e1203")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaSemana02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaSemana02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
